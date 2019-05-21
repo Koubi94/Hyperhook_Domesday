0de04eefcf220f17f87131f0b542d32b54172e96
Koubi94 <nils.jungjohann@live.de>
2019-05-21 17:18:49 +0200

    Implemented Standard AI

---
+++ Assets/Scripts/AI/EnemyManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static PlayerData m_Player;
+
+    private static List<Enemy> m_activeEnemies = new List<Enemy>();
+
+    public static void AddEnemy(Enemy _enemy)
+    {
+        if (m_activeEnemies.Contains(_enemy))
+        {
+            return;
+        }
+
+        if (m_Player == null)
+        {
+            m_Player = FindObjectOfType<PlayerData>();
+        }
+
+        _enemy.Instantiate(m_Player);
+        m_activeEnemies.Add(_enemy);
+    }
+
+    public static void RemoveEnemy(Enemy _enemy)
+    {
+        if (!m_activeEnemies.Contains(_enemy))
+        {
+            return;
+        }
+
+        m_activeEnemies.Remove(_enemy);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public float m_EyeHeight;
+
+    private void Awake()
+    {
+        
+    }
+
+    public Vector3 GetPosition()
+    {
+        return transform.position + Vector3.up * m_EyeHeight;
+    }
+}
