78486f78e1ae759aa4754dc12911df08c82a5fcb
Koubi94 <nils.jungjohann@live.de>
2019-05-22 08:20:12 +0200

    Added VFX to Weapon and Level

---
+++ Assets/Scripts/Weapon/SawBlade.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBlade : MonoBehaviour
+{
+    private Rigidbody m_rigidBody;
+
+    private void Awake()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_rigidBody.velocity = Vector3.zero;
+        m_rigidBody.useGravity = false;
+
+        //transform.position = other.ClosestPointOnBounds(transform.position);
+        Debug.Log(other.name);
+    }
+}
